9a9789b9bae89ad3c331908076f6a16534bb932d
Stefan Vlastuin <stefan.vlastuin@gmail.com>
2024-05-21 17:33:33 +0200

    add correlation

---
+++ stats/correlation.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+from read_data.read_data import get_dataframe
+
+
+def show_correlation(df):
+    pd.set_option('display.max_columns', None)  # Show all columns
+
+    standardized_df = (df - df.mean()) / df.std()
+    correlation_matrix = standardized_df.corr() # Uses Pearson correlation coefficient
+
+    plt.figure(figsize=(40, 30))
+    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', vmin=-1, vmax=1)
+    plt.title('Correlation Matrix')
+    plt.show()
+
+
+show_correlation(get_dataframe().iloc[:, 1:])
