4a870cd8c68b9a1449edda03aa63a5cb03b59551
Stefan Vlastuin <stefan.vlastuin@gmail.com>
2024-05-22 14:24:06 +0200

    improved organization with uni + multi

---
+++ logistic_regression/log_reg.py
@@ -0,0 +1,40 @@
+import numpy as np
+import statsmodels.api as sm
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+
+
+def apply_logistic_regression(x, y):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
+
+    # TODO: use k-fold cross validation
+    # TODO: balance the dataset
+
+    # Add constants (for intercept)
+    x_train_const = sm.add_constant(x_train)
+    x_test_const = sm.add_constant(x_test)
+
+    model = sm.Logit(y_train, x_train_const)
+    model_sm = model.fit(disp=0, maxiter=100)
+
+    # Model properties
+    p_values = model_sm.pvalues
+    pr_squared = model_sm.prsquared  # Use pseudo R-squared because that fits better with logistic regression
+    coefficients = model_sm.params
+    odds_ratios = np.exp(coefficients)
+
+    # Prediction metrics
+    y_pred_prob = model_sm.predict(x_test_const)
+    y_pred = (y_pred_prob > 0.5).astype(int)
+
+    accuracy = metrics.accuracy_score(y_test, y_pred)
+    precision = metrics.precision_score(y_test, y_pred, zero_division=0)
+    recall = metrics.recall_score(y_test, y_pred, zero_division=0)
+    f1 = metrics.f1_score(y_test, y_pred, zero_division=0)
+    confusion_matrix = metrics.confusion_matrix(y_test, y_pred)
+
+    return coefficients, odds_ratios, p_values, pr_squared, accuracy, precision, recall, f1, confusion_matrix
+
+
+def enough_values(v):
+    return v.where(lambda x: x > 0).count() >= 10
--- logistic_regression/univariate_statsmodels.py
+++ logistic_regression/univariate_statsmodels.py
@@ -1,13 +1,10 @@
-import numpy as np
 import pandas as pd
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
-import statsmodels.api as sm
 
+from logistic_regression.log_reg import enough_values, apply_logistic_regression
 from read_data.read_data import get_dataframe
 
 
-def univariate_all(df):
+def univariate(df):
     x = df.iloc[:, 1:-1]  # Features (leave out file names and target variable)
     x = x.apply(pd.to_numeric, errors='coerce').fillna(0)
     y = df['hasBug']  # Target variable
@@ -15,46 +12,11 @@ def univariate_all(df):
     # Do univariate logistic regression for each feature separately
     for feature in x.columns:
         if enough_values(df[feature]):
-            coefficient, odds_ratio, p_value, pr_squared, accuracy, precision, recall, f1 = univariate(x[[feature]], y)
-            print(feature, coefficient, odds_ratio, p_value, pr_squared, accuracy, precision, recall, f1)
+            coefficients, odds_ratios, p_values, pr_squared, accuracy, precision, recall, f1, confusion_matrix = apply_logistic_regression(x[[feature]], y)
+            print(feature, coefficients[feature], odds_ratios[feature], p_values[feature], pr_squared, accuracy, precision, recall, f1)
         else:
             print(feature, "has not enough non-zero values")
 
 
-def enough_values(v):
-    return v.where(lambda x: x > 0).count() >= 10
-
-
-def univariate(x, y):
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
-
-    # TODO: use k-fold cross validation
-    # TODO: balance the dataset
-
-    # Add constants (for intercept)
-    x_train_const = sm.add_constant(x_train)
-    x_test_const = sm.add_constant(x_test)
-
-    model = sm.Logit(y_train, x_train_const)
-    model_sm = model.fit(disp=0, maxiter=100)
-
-    # Model properties
-    p_value = model_sm.pvalues.iloc[1]
-    pr_squared = model_sm.prsquared  # Use pseudo R-squared because that fits better with logistic regression
-    coefficient = model_sm.params.iloc[1]
-    odds_ratio = np.exp(coefficient)
-
-    # Prediction metrics
-    y_pred_prob = model_sm.predict(x_test_const)
-    y_pred = (y_pred_prob > 0.5).astype(int)
-
-    accuracy = metrics.accuracy_score(y_test, y_pred)
-    precision = metrics.precision_score(y_test, y_pred, zero_division=0)
-    recall = metrics.recall_score(y_test, y_pred, zero_division=0)
-    f1 = metrics.f1_score(y_test, y_pred, zero_division=0)
-
-    return coefficient, odds_ratio, p_value, pr_squared, accuracy, precision, recall, f1
-
-
 data = get_dataframe()
-univariate_all(data)
+univariate(data)
