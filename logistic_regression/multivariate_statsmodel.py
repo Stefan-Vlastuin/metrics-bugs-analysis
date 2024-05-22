357de0f8c73232e7c96a65b54df74620c314a3ef
Stefan Vlastuin <stefan.vlastuin@gmail.com>
2024-05-22 15:24:13 +0200

    add todo

--- logistic_regression/multivariate_statsmodel.py
+++ logistic_regression/multivariate_statsmodel.py
@@ -2,4 +2,5 @@ from logistic_regression.log_reg import apply_logistic_regression
 
 
 def multivariate(x, y):
+    # TODO: use k-best features to select features
     return apply_logistic_regression(x, y)
