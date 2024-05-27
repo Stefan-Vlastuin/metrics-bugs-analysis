b3fbe394d0430364d585b7ca6c53f8c97d92a0a5
Stefan Vlastuin <stefan.vlastuin@gmail.com>
2024-05-27 14:44:16 +0200

    remove duplicate metrics

--- logistic_regression/multivariate.py
+++ logistic_regression/multivariate.py
@@ -5,6 +5,14 @@ from logistic_regression.log_reg import apply_logistic_regression
 
 
 def multivariate(x, y):
+    # In the current implementation, impurity is the same as using a field variable
+    # Therefore, we delete the impurity metrics, to prevent problems with multicollinearity
+    # TODO: remove when the implementations of these metrics have been improved
+    if 'MethodImpure' in x.columns:
+        del x['MethodImpure']
+    if 'MethodRatioImpure' in x.columns:
+        del x['MethodRatioImpure']
+
     selector = SelectKBest(f_regression, k=7)   # Number of baseline metrics; can substitute them for new metrics
     x_new = selector.fit_transform(x, y)
 
